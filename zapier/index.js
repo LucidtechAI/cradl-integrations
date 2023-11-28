f1b36d31d211c366696993c7c21e34792f4f095a
Torbjørn <ringholm@gmail.com>
2023-11-28 15:41:03 +0100

    Add links to app clients in descriptions

--- zapier/index.js
+++ zapier/index.js
@@ -16,13 +16,13 @@ const authentication = {
       key: 'app_client_id', 
       type: 'string', 
       required: true,
-      helpText: 'App client ID'
+      helpText: 'App client ID is found on the [App clients page](https://app.cradl.ai/appclients)'
     },
     { 
       key: 'app_client_secret', 
       type: 'password', 
       required: true,
-      helpText: 'App client secret'
+      helpText: 'App client secret is found on the [App clients page](https://app.cradl.ai/appclients)'
     },
   ],
   connectionLabel: '{{bundle.inputData.name}}', 
