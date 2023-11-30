0b347932453abead07106f3d979687ea155909b3
Torbjørn <ringholm@gmail.com>
2023-11-30 10:50:32 +0100

    Fixes for workflow complete action

--- zapier/triggers/workflowComplete.js
+++ zapier/triggers/workflowComplete.js
@@ -4,12 +4,12 @@ const setWebhookUrl = async(z, bundle) => {
     const getWorkflowResponse = await cradlApi.getWorkflow(z, bundle.inputData.workflowId)
     metadata = getWorkflowResponse.data.metadata
     metadata.outputWebhookUri = bundle.targetUrl
-    const updateWorkflowResponse = await cradlApi.updateWorkflow(z, bundle.inputData.workflowId, metadata)
+    cradlApi.updateWorkflow(z, bundle.inputData.workflowId, metadata)
 
     const getTransitionResponse = await cradlApi.getTransition(z, metadata.postprocessTransitionId)
     parameters = getTransitionResponse.data.parameters
     parameters.environment.WEBHOOK_URI = bundle.targetUrl
-    const updateTransitionResponse = await cradlApi.updateTransition(z, metadata.postprocessTransitionId, parameters) 
+    cradlApi.updateTransition(z, metadata.postprocessTransitionId, parameters) 
     
     // Return data needed for deleting the webhook later.
     return {
@@ -19,27 +19,21 @@ const setWebhookUrl = async(z, bundle) => {
   };
   
   const deleteWebhookUrl = async (z, bundle) => {
-    const getWorkflowResponse = await cradlApi.getWorkflow(z, bundle.subscribeData.workflowId)
-    metadata = getWorkflowResponse.data.metadata
-    delete metadata.outputWebhookUri
-    const updateWorkflowResponse = await cradlApi.updateWorkflow(z, bundle.subscribeData.workflowId, metadata)
+    cradlApi.updateWorkflow(z, bundle.subscribeData.workflowId, {outputWebhookUri: null})
 
     const getTransitionResponse = await cradlApi.getTransition(z, bundle.subscribeData.transitionId)
     parameters = getTransitionResponse.data.parameters
-    delete parameters.environment.WEBHOOK_URI
     const updateTransitionResponse = await cradlApi.updateTransition(z, bundle.subscribeData.transitionId, parameters) 
     return updateTransitionResponse;
   };
   
   const processWebhookPayload = (z, bundle) => {
-    return bundle.cleanedRequest;
+    return [bundle.cleanedRequest];
   };
   
   const listCompleted = async (z, bundle) => {
     const getSuccessfulWorkflowExecutionsResponse = await cradlApi.getSuccessfulWorkflowExecutions(z, bundle.inputData.workflowId)
-    console.log(getSuccessfulWorkflowExecutionsResponse)
     executions = getSuccessfulWorkflowExecutionsResponse.data.executions
-    console.log(executions)
     outputs = executions.map((execution) => {return execution.output})
     return outputs
   };
