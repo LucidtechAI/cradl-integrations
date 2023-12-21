552f1b8eb5e7ec101871cb6db42da714b12ad184
Magnus <mag.aars@gmail.com>
2023-12-21 14:32:19 +0100

    Add UIPath integration (#5)
    * Initial commit for UIPath integration
    
    * remove ListFlows and add all the others
    
    * Add missing designers
    
    * get the columns right
    
    * remove optional arguments
    
    * remove buggy xml
    
    * Add readme
    
    * Update readme
    
    * Rename
    
    * Update project
    
    * Fix Readme
    
    * Fix Readme
    
    * Add some space

---
+++ UiPath/CradlAI/CradlAI/CradlAI.Activities.Design/Designers/ListProcessedDocumentsDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace CradlAI.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ListProcessedDocumentsDesigner.xaml
+    /// </summary>
+    public partial class ListProcessedDocumentsDesigner
+    {
+        public ListProcessedDocumentsDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UiPath/CradlAI/CradlAI/CradlAI.Activities.Design/Designers/MarkRunAsCompletedDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace CradlAI.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for MarkRunAsCompletedDesigner.xaml
+    /// </summary>
+    public partial class MarkRunAsCompletedDesigner
+    {
+        public MarkRunAsCompletedDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
